0a6e5ae0a801fd0de80e3fec70156dac375f7b89
ddang9390 <dnld1ng@yahoo.com>
2019-05-21 16:21:21 -0400

    make num actually a number

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,6 +1,6 @@
 var answer = Math.floor(Math.random() * 101);
 var num;
-while((num = prompt("Enter a number")) != answer ){
+while((num =Number(prompt("Enter a number"))) !== answer ){
     if(num < answer){
         alert("Too low");
     }
