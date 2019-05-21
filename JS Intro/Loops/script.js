e903647a26f9b58058913a2ceb2db7e5238a7495
ddang9390 <dnld1ng@yahoo.com>
2019-05-21 16:38:49 -0400

    for loop exercise

--- JS Intro/Loops/script.js
+++ JS Intro/Loops/script.js
@@ -28,3 +28,27 @@ while(num4 <= 50){
     }
     num4++;
 }
+
+console.log("For Loop Time");
+console.log("Problem 1");
+for(var num1 = -10; num1 <= 19; num1++){
+    console.log(num1);
+}
+console.log("Problem 2");
+for(var num2 = 10; num2 <= 40; num2++){
+    if((num2 % 2) === 0){
+        console.log(num2);
+    }
+}
+console.log("Problem 3");
+for(var num3 = 300; num3 <= 333; num3++){
+    if((num3 % 2) !== 0){
+        console.log(num3);
+    }
+}
+console.log("Problem 4");
+for(num4 = 5; num4 <= 50; num4++){
+    if(((num4 % 5) === 0) && ((num4 % 3) === 0)){
+        console.log(num4);
+    }
+}
