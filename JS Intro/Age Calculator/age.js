c23dcf25c9f3210704774cb43ce998b9b8545e94
ddang9390 <dnld1ng@yahoo.com>
2019-05-21 15:26:48 -0400

    JS intro exercises

---
+++ JS Intro/Age Calculator/age.js
@@ -0,0 +1,4 @@
+var years = prompt("How old are you?");
+var days = years * 365;
+
+alert("You've been living for " + days + " days");
---
+++ JS Intro/stalker/js-stalker.js
@@ -0,0 +1,9 @@
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+alert("Greetings " + fname + " " + lname);
+alert("You are " + age + " years old");
+
+console.log("Greetings " + fname + " " + lname);
+console.log("You are " + age + " years old");
